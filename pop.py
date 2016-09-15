58e6fc636a672062009cde705e72ee2aea615a5a
achencoms <achencoms@gmail.com>
2016-09-15 18:40:11 -0400

    Try it out

--- pop.py
+++ pop.py
@@ -5,17 +5,18 @@ oc = open("occupations.csv","r").readlines()[1:-1]
 oc = csv.reader(oc)
 dictionary = {}
 
-
 for job in oc:
-    dictionary[float(job[1])] = job[0]
+    dictionary[job[0]] = float(job[1])
+
 
 def jobme():
     counter = 0
     choose = random.uniform(0,99.8)
     for i in dictionary:
-        counter += i
-        if choose < counter:
-            return dictionary[i]
+        counter += dictionary[i]
+        if choose <= counter:
+            return i
         
 print(jobme())
+
 
